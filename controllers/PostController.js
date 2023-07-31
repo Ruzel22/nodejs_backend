cd7a476b9616e0575ab596d2b8a13eedf53d8173
Ruzel <ruzeldenf222@gmail.com>
2023-07-31 23:49:28 +0300

    fix

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -23,7 +23,7 @@ export const create = async (req, res, next) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(','),
       user: req.userId,
     });
 
